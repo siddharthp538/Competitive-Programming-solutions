9d9918977fe20515ace25df43fa8e566ccda8663
Siddharth Pandey <svpandey86@gmail.com>
2017-07-01 23:46:41 +0530

    Update SNCOUP.java

--- src/SiddharthMay17/SNCOUP.java
+++ src/SiddharthMay17/SNCOUP.java
@@ -1,19 +1,22 @@
 package SiddharthMay17;
-
+/* Problem Name: Snakes and their coup against The mongoose 
+   Problem Code: SNCOUP
+   Contest: SnackDown Pre-Elimination Round B 2017
+   Link: https://www.codechef.com/SNCKPB17/problems/SNCOUP
+   author: siddharthp538
+*/
 import java.util.*;
 import java.io.*;
 
 class SNCOUP {
 	public static void main(String[] args) {
-		InputReader s = new InputReader(System.in);
-
-		StringBuilder sb = new StringBuilder();
+		Scanner s = new Scanner(System.in);
 		int t = s.nextInt();
 		while (t-- > 0) {
 			int n = s.nextInt();
 			char[][] a = new char[2][n];
 			for (int i = 0; i < 2; i++) {
-				String str = s.readString();
+				String str = s.next();
 				for (int j = 0; j < n; j++) {
 					a[i][j] = str.charAt(j);
 				}
@@ -53,117 +56,3 @@ class SNCOUP {
 
 }
 
-class InputReader {
-
-	private final InputStream st;
-	private final byte[] buf = new byte[8192];
-	private int cc, sc;
-	private SpaceCharFilter f;
-
-	public InputReader(InputStream st) {
-		this.st = st;
-	}
-
-	public int t() {
-		if (sc == -1)
-			throw new InputMismatchException();
-		if (cc >= sc) {
-			cc = 0;
-			try {
-				sc = st.read(buf);
-			} catch (IOException e) {
-				throw new InputMismatchException();
-			}
-			if (sc <= 0)
-				return -1;
-		}
-		return buf[cc++];
-	}
-
-	public int nextInt() {
-		int c = t();
-		while (isSpaceChar(c)) {
-			c = t();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = t();
-		}
-		int res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = t();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public long nextLong() {
-		int c = t();
-		while (isSpaceChar(c)) {
-			c = t();
-		}
-		int sgn = 1;
-		if (c == '-') {
-			sgn = -1;
-			c = t();
-		}
-		long res = 0;
-		do {
-			res *= 10;
-			res += c - '0';
-			c = t();
-		} while (!isSpaceChar(c));
-		return res * sgn;
-	}
-
-	public int[] nextIntArray(int n) {
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = nextInt();
-		}
-		return a;
-	}
-
-	public String readString() {
-		int c = t();
-		while (isSpaceChar(c)) {
-			c = t();
-		}
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = t();
-		} while (!isSpaceChar(c));
-		return res.toString();
-	}
-
-	public String nextLine() {
-		int c = t();
-		while (isSpaceChar(c))
-			c = t();
-		StringBuilder res = new StringBuilder();
-		do {
-			res.appendCodePoint(c);
-			c = t();
-		} while (!isEndOfLine(c));
-		return res.toString();
-	}
-
-	public boolean isSpaceChar(int c) {
-		if (f != null)
-			return f.isSpaceChar(c);
-		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-	}
-
-	private boolean isEndOfLine(int c) {
-		return c == '\n' || c == '\r' || c == -1;
-	}
-
-	public interface SpaceCharFilter {
-		public boolean isSpaceChar(int ch);
-	}
-}
-
-// Comments
