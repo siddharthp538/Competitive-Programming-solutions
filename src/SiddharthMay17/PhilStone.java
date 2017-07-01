c24988ea74cf6d8d06c9d37fc30528cca479cd23
Siddharth Pandey <svpandey86@gmail.com>
2017-07-01 23:53:10 +0530

    Update PhilStone.java

--- src/SiddharthMay17/PhilStone.java
+++ src/SiddharthMay17/PhilStone.java
@@ -1,11 +1,18 @@
 package SiddharthMay17;
-
-import java.util.Scanner;
+/* Problem Name: Philospher's Stone 
+   Problem Code: BYTESM2
+   Contest: Practice-SPOJ-DP
+   Link: https: http://www.spoj.com/problems/BYTESM2/
+   author: siddharthp538
+*/
+import java.util.*;
+import java.io.*;
 
 class PhilStone {
 	public static void main(String[] args) {
 
-		Scanner s = new Scanner(System.in);
+		InputReader s = new InputReader(System.in);
+		PrintWriter w=new PrintWriter(System.out);
 		int t = s.nextInt();
 		while (t-- > 0) {
 			int n = s.nextInt(), m = s.nextInt();
@@ -28,10 +35,10 @@ class PhilStone {
 				ans = Math.max(dp[0][i], ans);
 
 			}
-			System.out.println(ans);
+			w.println(ans);
 
 		}
-		// w.close();
+		 w.close();
 	}
 
 	public static int solve(int i, int j, int n, int m, int sum, int[][] a, int dp[][]) {
@@ -63,10 +70,118 @@ class PhilStone {
 		return true;
 	}
 
-	static int solve1(int n, int ans) {
-		if (n == 0)
-			return ans;
-		return solve1(n / 10, (ans * 10 + n % 10));
+	
+}
+
+class InputReader {
+
+	private final InputStream st;
+	private final byte[] buf = new byte[8192];
+	private int cc, sc;
+	private SpaceCharFilter f;
+
+	public InputReader(InputStream st) {
+		this.st = st;
 	}
 
+	public int t() {
+		if (sc == -1)
+			throw new InputMismatchException();
+		if (cc >= sc) {
+			cc = 0;
+			try {
+				sc = st.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (sc <= 0)
+				return -1;
+		}
+		return buf[cc++];
+	}
+
+	public int nextInt() {
+		int c = t();
+		while (isSpaceChar(c)) {
+			c = t();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = t();
+		}
+		int res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = t();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public long nextLong() {
+		int c = t();
+		while (isSpaceChar(c)) {
+			c = t();
+		}
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = t();
+		}
+		long res = 0;
+		do {
+			res *= 10;
+			res += c - '0';
+			c = t();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public int[] nextIntArray(int n) {
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = nextInt();
+		}
+		return a;
+	}
+
+	public String readString() {
+		int c = t();
+		while (isSpaceChar(c)) {
+			c = t();
+		}
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = t();
+		} while (!isSpaceChar(c));
+		return res.toString();
+	}
+
+	public String nextLine() {
+		int c = t();
+		while (isSpaceChar(c))
+			c = t();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = t();
+		} while (!isEndOfLine(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		if (f != null)
+			return f.isSpaceChar(c);
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	private boolean isEndOfLine(int c) {
+		return c == '\n' || c == '\r' || c == -1;
+	}
+
+	public interface SpaceCharFilter {
+		public boolean isSpaceChar(int ch);
+	}
 }
