b7d1d86667fdb58489dbaf33c94ceb1453247f0e
Siddharth Pandey <svpandey86@gmail.com>
2017-07-01 23:44:00 +0530

    Update SEAVOTE.java

--- src/SiddharthMay17/SEAVOTE.java
+++ src/SiddharthMay17/SEAVOTE.java
@@ -1,7 +1,12 @@
 package SiddharthMay17;
-
+/* Problem Name: Sereja and Votes 
+   Problem Code: SEAVOTE
+   Contest: Practice-Medium
+   Link: https://www.codechef.com/problems/SEAVOTE/
+   author: siddharthp538
+*/
 import java.util.Scanner;
-
+ 
 class SEAVOTE {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -10,23 +15,26 @@ class SEAVOTE {
 			calc(s);
 		}
 	}
-
+ 
 	public static void calc(Scanner s) {
 		int n = s.nextInt();
 		int[] a = new int[n];
-		long sum = 0;
+		long sum = 0,zero=0;
 		for (int i = 0; i < n; i++) {
 			a[i] = s.nextInt();
 			sum += a[i];
+			if(a[i]==0)
+			zero++;
+			
 		}
 		if (sum < 100)
 			System.out.println("NO");
 		else {
 			sum -= 100;
 			sum++;
-			System.out.println(sum <= n ? "YES" : "NO");
+			System.out.println(sum <= (n-zero) ? "YES" : "NO");
 		}
-
+ 
 	}
-
-}
+ 
+} 
